43673284ecfcd4cc5c7acf9709b9bf7e32922542
alvinyanson <a.yanson@arcanys.com>
2024-09-24 19:11:35 +0800

    feat: register page

--- StravaClone.Web/Controllers/AccountController.cs
+++ StravaClone.Web/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StravaClone.Web.Data;
 using StravaClone.Web.Models;
 using StravaClone.Web.ViewModels;
 
@@ -29,21 +30,21 @@ namespace StravaClone.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginViewModel)
         {
-            if(!ModelState.IsValid) return View(loginViewModel);
+            if (!ModelState.IsValid) return View(loginViewModel);
 
             var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
 
-            if(user != null)
+            if (user != null)
             {
                 // User is found, check password
                 var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
 
-                if(passwordCheck)
+                if (passwordCheck)
                 {
                     // Password correct, signed in
                     var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
 
-                    if(result.Succeeded)
+                    if (result.Succeeded)
                     {
                         return RedirectToAction("Index", "Race");
                     }
@@ -56,8 +57,53 @@ namespace StravaClone.Web.Controllers
 
             // User not found
             TempData["Error"] = "Wrong credentials. Please, try again.";
-            
+
             return View(loginViewModel);
         }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use.";
+                return View(registerViewModel);
+            }
+
+            var newUser = new AppUser
+            {
+                Email = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress,
+            };
+
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+
+            return RedirectToAction("Index", "Race");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Race");
+        }
+
     }
 }
---
+++ StravaClone.Web/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StravaClone.Web.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required.")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
