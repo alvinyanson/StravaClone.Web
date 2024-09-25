622d32c51eee4b08db56554db3536f7c4745f7d1
alvinyanson <a.yanson@arcanys.com>
2024-09-25 12:35:26 +0800

    feat: list of users

---
+++ StravaClone.Web/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StravaClone.Web.Interfaces;
+using StravaClone.Web.ViewModels;
+
+namespace StravaClone.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet("users")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsers();
+
+            List<UserViewModel> result = new List<UserViewModel>();
+
+            foreach (var user in users)
+            {
+                var userViewModel = new UserViewModel()
+                {
+                    Id = user.Id,
+                    Username = user.UserName,
+                    Pace = user.Pace,
+                    MileAge = user.MileAge
+                };
+
+                result.Add(userViewModel);
+            }
+
+            return View(result);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(string id)
+        {
+            var user = await _userRepository.GetUserById(id);
+
+            var userDetailViewModel = new UserDetailViewModel()
+            {
+                Id = user.Id,
+                Username = user.UserName,
+                Pace = user.Pace,
+                MileAge = user.MileAge
+            };
+
+            return View(userDetailViewModel);
+        }
+    }
+}
